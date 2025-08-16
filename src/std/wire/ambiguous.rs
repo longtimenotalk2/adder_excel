2473ae0c2c9746b4b1180c5c372d2858f7414bbf
lihaiwei <lihaiwei19921122@163.com>
2025-08-16 15:10:49 +0800

    amb change to enum

--- src/std/wire/ambiguous.rs
+++ src/std/wire/ambiguous.rs
@@ -1,13 +1,7 @@
 use crate::std::wire::{AmbiguousWire, Wire};
 
 impl Wire {
-    pub fn to_ambiguous(&self) -> AmbiguousWire {
-        AmbiguousWire {
-            flag : self.flag,
-            is_neg : Some(self.is_neg),
-            is_ab_xr : Some(self.is_ab_xr),
-            index : self.index,
-            len : Some(self.len),
-        }
+    pub fn to_ambiguous(self) -> AmbiguousWire {
+        AmbiguousWire::Precise(self)
     }
 }
